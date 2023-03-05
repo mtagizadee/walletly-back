b0ae7b101144b594dca56352a538689bd94cf305
mtagizadee <magatagi0@gmail.com>
2023-03-05 20:14:50 +0400

    deployment test

--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { WalletsService } from './wallets.service';
 import { WalletsController } from './wallets.controller';
 
+// this is a module for wallets
 @Module({
   providers: [WalletsService],
   controllers: [WalletsController],
